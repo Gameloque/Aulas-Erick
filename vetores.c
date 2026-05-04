1e96e60bca963901cbf42bf0a1424ea6400cfa93
gameloque <gabrielhenriqueff1448@gmail.com>
2026-05-04 11:28:31 -0300

    Vetores

---
+++ vetores.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int quantidade_notas;
+    printf("Digite a quantidade de notas: ");
+    scanf("%d", &quantidade_notas);
+    float notas[quantidade_notas]; // De 0 a 1 (indice)
+    float soma;
+    
+    for(int i = 0; i < quantidade_notas; i++){
+        printf("Digite a sua nota %d: ", i+1);
+        scanf("%f", &notas[i]);
+        soma += notas[i];
+        
+    }
+    printf("A media do aluno e: %.2f\n", soma/quantidade_notas);
+  
+
+return 0;
+}
